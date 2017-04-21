a43281d6636c0f640335af259b6a9ab20c6f29e6
arunkumarsomu <kumar_sundaram82@yahoo.com>
2017-04-21 14:11:58 -0400

    Create BankDB.java

---
+++ BankDB.java
@@ -0,0 +1,18 @@
+// Simplified Version of the Bank DataBase
+
+public class BankDB {
+
+	public int getBalance(int account) {
+		// this method is not going to do anything, 
+		// we will need to mock it up.  It returns a 
+		// -1 which will assume for sake of the problem is
+		// not a legal bank balance.
+	return 1;
+}
+	
+	public AccountStatus getStatus() {
+	return null;
+}
+	
+	
+}
